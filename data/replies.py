2b1b136e802d114796e0ad81bbb9c54f52b92a55
Quite Quiet <femtontusen@gmail.com>
2015-05-25 19:59:08 +0200

    add replies.py to complete ~ask

---
+++ data/replies.py
@@ -0,0 +1,8 @@
+lines = [
+    'Needs more Assault Vest.',
+    'Only with Assault Vest equipped.',
+    'Yes, because you have an Assault Vest.',
+    '#BT approved.',
+    '#BlameTruth would be so proud.',
+    "I don't like it, there's no Assault Vest.'
+]
