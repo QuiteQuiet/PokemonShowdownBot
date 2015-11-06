579b9ef1380530032b2df5c5068116ffb3b9f533
quitequiet <femtontusen@gmail.com>
2015-11-06 07:30:49 +0000

    update links

--- data/links.py
+++ data/links.py
@@ -4,7 +4,7 @@ Links = {
     'ou':'http://www.smogon.com/forums/threads/3536420/',
     'uu':'http://www.smogon.com/forums/threads/3533364/',
     'ru':'http://www.smogon.com/forums/threads/3538036/',
-    'nu':'http://www.smogon.com/forums/threads/3523692/',
+    'nu':'http://www.smogon.com/forums/threads/3553680/',
     'pu':'http://www.smogon.com/forums/threads/3528743/',
     'lc':'http://www.smogon.com/forums/threads/3496013/'
     },
@@ -13,7 +13,7 @@ Links = {
     'ou':'http://www.smogon.com/forums/threads/3538596/',
     'uu':'http://www.smogon.com/forums/threads/3538856/',
     'ru':'http://www.smogon.com/forums/threads/3538971/',
-    'nu':'http://www.smogon.com/forums/threads/3537418/',
+    'nu':'http://www.smogon.com/forums/threads/3554741/',
     'pu':'http://www.smogon.com/forums/threads/3523350/',
     'lc':'http://www.smogon.com/forums/threads/3505710/'
     },
