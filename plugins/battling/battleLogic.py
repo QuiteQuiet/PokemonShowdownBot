da2a2cb3ab134c00d9cda3d041c297a4c3960c60
quitequiet <femtontusen@gmail.com>
2016-08-03 14:38:11 +0000

    More battle error logging

--- plugins/battling/battleLogic.py
+++ plugins/battling/battleLogic.py
@@ -136,6 +136,7 @@ def getLead(team, opposing):
         options = [poke for poke,score in scores.items() if score == m]
         return team[options[randint(0,len(options)-1)]].teamSlot
     except ValueError:
+        print('WARNING: Failed to pick proper lead, using random.')
         return randint(1,6)
 
 def calcScore(move, mon, opponents):
