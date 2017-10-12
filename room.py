e674d0d4680cfb7a395f432d05a0bddbec7563a9
William Granados <me@wgma00.me>
2017-10-12 03:21:47 -0400

    added docstrings to room-based commands

--- room.py
+++ room.py
@@ -106,6 +106,18 @@ class Room:
 
 # Commands
 def leaveroom(bot, cmd, room, params, user):
+    """ Independent command for making this bot leave a room.
+
+    Args:
+        bot: PokemonShowdownBot, the instance of PokemonShowdownBot that called this function.
+        cmd: string, the command that was send.
+        room: Room, the room object that the command was sent from.
+        params: string, optional parameter like room name, if left empty
+                function attempts to leave room where this command was invoked.
+        user: User, the user object of the user who sent the command.
+    Returns:
+        ReplyObject.
+    """
     reply = ReplyObject()
     params = bot.removeSpaces(params)
     if not params: params = room.title
@@ -114,6 +126,19 @@ def leaveroom(bot, cmd, room, params, user):
     return reply.response('Could not leave room: {r}'.format(r = params))
 
 def allowgames(bot, cmd, room, params, user):
+    """ Independent command for changing permissions for games in this room.
+    
+    Reserved for room owners. They can decide to allow games/activities in their room. 
+
+    Args:
+        bot: PokemonShowdownBot, the instance of PokemonShowdownBot that called this function.
+        cmd: string, the command that was send.
+        room: Room, the room object that the command was sent from.
+        params: string, required parameter indicating the status of games in this room.
+        user: User, the user object of the user who sent the command.
+    Returns:
+        ReplyObject.
+    """
     reply = ReplyObject(True)
     if not user.hasRank('#'): return reply.response('You do not have permission to change this. (Requires #)')
     if room.isPM: return reply.response("You can't use this command in a pm.")
@@ -129,12 +154,39 @@ def allowgames(bot, cmd, room, params, user):
     return reply.response('{param} is not a supported parameter'.format(param = params))
 
 def tour(bot, cmd, room, params, user):
+    """ Independent command for initiating tours in this room.
+   
+    This is only possible for rooms where this bot has at least '@' rank. Intended
+    trusted users who do not have the required room rank.
+
+    Args:
+        bot: PokemonShowdownBot, the instance of PokemonShowdownBot that called this function.
+        cmd: string, the command that was send.
+        room: Room, the room object that the command was sent from.
+        params: string, parameter(s) you'd give a normal /tour command on showdown. 
+        user: User, the user object of the user who sent the command.
+    Returns:
+        ReplyObject.
+    """
     reply = ReplyObject('', True, True, True)
     if room.isPM: return reply.response("You can't use this command in a pm.")
     if not room.isWhitelisted(user): return reply.response('You are not allowed to use this command. (Requires whitelisting by a Room Owner)')
     if not bot.canStartTour(room): return reply.response("I don't have the rank required to start a tour :(")
     return reply.response('/tour {rest}\n/modnote From {user}'.format(rest = params, user = user.name))
 def tourwl(bot, cmd, room, params, user):
+    """ Independent command for a user to tours whitelist.
+    
+    Reserved for room owners.
+    
+    Args:
+        bot: PokemonShowdownBot, the instance of PokemonShowdownBot that called this function.
+        cmd: string, the command that was send.
+        room: Room, the room object that the command was sent from.
+        params: string, the name of the user. 
+        user: User, the user object of the user who sent the command.
+    Returns:
+        ReplyObject.
+    """
     reply = ReplyObject('', True)
     if not user.hasRank('#'): return reply.response('You do not have permission to change this. (Requires #)')
     target = bot.toId(params)
@@ -142,6 +194,19 @@ def tourwl(bot, cmd, room, params, user):
     bot.saveDetails()
     return reply.response('{name} added to the whitelist in this room.'.format(name = params))
 def untourwl(bot, cmd, room, params, user):
+    """ Independent command for removing a user from the tours whitelist.
+    
+    Reserved for room owners.
+    
+    Args:
+        bot: PokemonShowdownBot, the instance of PokemonShowdownBot that called this function.
+        cmd: string, the command that was send.
+        room: Room, the room object that the command was sent from.
+        params: string, the name of the user. 
+        user: User, the user object of the user who sent the command.
+    Returns:
+        ReplyObject.
+    """
     reply = ReplyObject('', True)
     if not user.hasRank('#'): return reply.response('You do not have permission to change this. (Requires #)')
     target = bot.toId(params)
