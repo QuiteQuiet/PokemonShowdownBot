71501b85e537a2837ac3b941c72d267129734c9e
Quite Quiet <femtontusen@gmail.com>
2018-07-10 18:08:11 +0000

    Add more Assault Vest replies

--- data/replies.py
+++ data/replies.py
@@ -3,5 +3,7 @@ Lines = [
     'Only with Assault Vest equipped.',
     'Yes, because you have an Assault Vest.',
     '#BT approved.',
-    "I don't like it, there's no Assault Vest."
+    "I don't like it, there's no Assault Vest.",
+    "Doesn't seem like there's any Assault Vest here, so probably not.",
+    'If Assault Vest is included: Unquestionably yes, otherwise: probably not.'
 ]
